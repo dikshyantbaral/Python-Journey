722206ad777972f4e929a0c8c021d2e7b31a573b
Dikshyant Baral <baraldikshyant@gmail.com>
2026-01-06 07:43:18 +0545

    lists completed

--- Chapter 4/02_list.py
+++ Chapter 4/02_list.py
@@ -2,3 +2,12 @@ friends = ["Apple", "Orange", 5,667,.06,False,"Noob"]
 print(friends)
 friends.append("Harry")#//to add at the end
 print(friends)
+l1 = [1,34,6,5,8,9]
+#l1.sort()
+#print(l1)
+#l1.reverse()
+#print(l1)
+#l1.insert(4,69) #(index, number)
+print(l1)
+l1.pop(3)#to delete the number
+print(l1)
