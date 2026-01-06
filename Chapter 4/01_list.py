6f8f45709a5f24aaee70cd1ebdc52c291bb1630f
Dikshyant Baral <baraldikshyant@gmail.com>
2026-01-06 07:31:47 +0545

    chapter 4

---
+++ Chapter 4/01_list.py
@@ -0,0 +1,5 @@
+friends = ["Apple", "Orange", 5,667,.06,False,"Noob"]
+print(friends[0])
+friends[0] = "Banana" #unlike strings lists are mutable
+print(friends[5])
+print(friends[1:4])
---
+++ Chapter 4/02_list.py
@@ -0,0 +1,4 @@
+friends = ["Apple", "Orange", 5,667,.06,False,"Noob"]
+print(friends)
+friends.append("Harry")#//to add at the end
+print(friends)
