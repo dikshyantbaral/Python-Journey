293c5530593ee8fd7b5bd98bb004bf6535d1d6c7
Dikshyant Baral <baraldikshyant@gmail.com>
2026-01-30 17:48:52 +0545

    Chapter 5

---
+++ Chapter 5/Practise Set/problem06.py
@@ -0,0 +1,3 @@
+# The values entered later will be updated
+
+s = {8,7,12, "Harry",[1,2]}
---
+++ Chapter 6/01_conditional.py
@@ -0,0 +1,7 @@
+a = int(input("Enter your age: "))
+if (a>=18):
+    print("You can vote")
+elif(a<0):
+    print("Invalid Age!")
+else:
+    print("You cannot vote")
