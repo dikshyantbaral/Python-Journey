b7aac8906491e6beee4dcd4845546b351d5f9eec
Dikshyant Baral <baraldikshyant@gmail.com>
2026-02-22 15:28:21 +0545

    Snake Water Gun

---
+++ Project 1/main_shortened.py
@@ -0,0 +1,20 @@
+''' 
+1 for snake
+-1 for water
+0 for gun
+'''
+import random
+
+computer = random.choice([-1,0,1])
+youstr = input("Enter your choice: ")
+youDict = {"s":1, "w": -1,"g":0}
+reverseDict = {1: "Snake",-1:"Water",0:"Gun"}
+you = youDict[youstr]
+print(f"You chose {reverseDict[you]}\n Computer chose {reverseDict[computer]}")
+if(computer == you):
+    print("Its a draw")
+else:
+    if((computer - you)==-1 or (computer - you)==2):
+        print("You lose!")
+    else:
+        print("You win!")
